5e9a35de0bfce7a55582bceae6a488ec28c9bf82
yisavd <yisavd@hallgato.nje.hu>
2026-04-10 17:58:28 +0200

    still testing..

--- fetch/kapcsolat.php
+++ fetch/kapcsolat.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $dbname = "pizzarendeles";
 $user = "pizzarendeles";
-$pass = "123456";
+$pass = "Zsombor29-admin";
 $charset = "utf8mb4";
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
